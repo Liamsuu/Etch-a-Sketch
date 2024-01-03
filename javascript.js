6b869fae0886d9cc5be612b978fa65a4f7666f43
Liam <liamsuu14974@gmail.com>
2024-01-03 18:49:48 +0000

    Add simple mouseover grid effect.
    I will need to change the effect so that only the pixel the mouse is
    over will change colour - thus leaving a 'trail'.

--- javascript.js
+++ javascript.js
@@ -1,7 +1,15 @@
 const container = document.querySelector(".container");
 
+// will create 16 boxes and place them inside the main container
 for (let x = 0; x < 16; x++) {
   const grid = document.createElement("div");
   container.appendChild(grid);
-  //grid.textContent = "testing";
 }
+
+const grids = document.querySelectorAll(".container div");
+
+grids.forEach((grid) => {
+  grid.addEventListener("mouseover", () => {
+    grid.style.background = "red";
+  });
+});
