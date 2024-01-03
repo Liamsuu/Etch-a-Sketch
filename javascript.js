70d8039d7ba898f1b384f569dd17994405d77bd9
Liam <liamsuu14974@gmail.com>
2024-01-03 20:37:34 +0000

    Add button to change number of grid squares
    I need to find a way to get a perfect even number of squares going
    sideways and upwards like 10*10 no matter the size.

--- javascript.js
+++ javascript.js
@@ -1,15 +1,33 @@
 const container = document.querySelector(".container");
 
-// will create 16 boxes and place them inside the main container
+// will create 16 boxes and place them inside the main container as initial value
 for (let x = 0; x < 16; x++) {
   const grid = document.createElement("div");
   container.appendChild(grid);
 }
-
 const grids = document.querySelectorAll(".container div");
+gridBackgroundChange();
 
-grids.forEach((grid) => {
-  grid.addEventListener("mouseover", () => {
-    grid.style.background = "red";
-  });
+const button = document.querySelector("button");
+button.addEventListener("click", () => {
+  let user_size = +prompt("Choose grid size(1- 100): ");
+  if (typeof user_size === "number" && user_size > 0 && user_size < 101) {
+    grids.forEach((grid) => {
+      grid.remove();
+    });
+    for (let x = 0; x < user_size; x++) {
+      const grid = document.createElement("div");
+      container.appendChild(grid);
+    }
+    gridBackgroundChange();
+  }
 });
+
+function gridBackgroundChange() {
+  const grids = document.querySelectorAll(".container div");
+  grids.forEach((grid) => {
+    grid.addEventListener("mouseover", () => {
+      grid.style.background = "red";
+    });
+  });
+}
