f34b7603d8de0b45466d067635d7ccae86e4e8a6
Liam <liamsuu14974@gmail.com>
2024-01-03 18:16:16 +0000

    Add 16 even grids to container

---
+++ javascript.js
@@ -0,0 +1,7 @@
+const container = document.querySelector(".container");
+
+for (let x = 0; x < 16; x++) {
+  const grid = document.createElement("div");
+  container.appendChild(grid);
+  //grid.textContent = "testing";
+}
